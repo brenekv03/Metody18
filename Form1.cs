7c9a022ae1b6cb6a7ee5521dd92790e1ceebcf72
brenekv.03 <brenekv.03@A321-15>
2022-06-09 13:03:28 +0200

    definovani

---
+++ Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Metody18
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[] poleretezcu = { "pes", "skakal", "pres", "oves" };
+        private static bool Existuje(string poleretezcu)
+        {
+            for(int i = 0; i < poleretezcu.Length;i++)
+            {
+                
+            }
+        }
+    }
+}
